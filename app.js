0be24d94464ccba97144f1f1ca04db24baa0ca00
Matt Wong <mattwong59@gmail.com>
2019-03-26 22:56:36 -0700

    Add logic to play game

--- app.js
+++ app.js
@@ -10,29 +10,45 @@ const scissors_div = document.getElementById('scissors');
 
 
 function getCompChoice() {
-    const choices = ['rock', 'paper', 'scissors'];
-    let random = Math.floor(Math.random() * 3);
-    console.log(choices[random]);
-    return choices[random];
+    const choices = ['r', 'p', 's'];
+    const randomNum = Math.floor(Math.random() * 3);
+    return choices[randomNum];
 }
 
 getCompChoice();
 
 function game(userChoice) {
-    return userChoice === 'rock' ? console.log('OH YEAH') : console.log("wan wan")
+    let compChoice = getCompChoice();
+    switch(userChoice + compChoice) {
+        case 'rs':
+        case 'pr':
+        case 'sp':
+            console.log('You win!',userChoice, compChoice)
+            break;
+        case 'ps':
+        case 'rp':
+        case 'sr':
+            console.log('You lose.',userChoice, compChoice)
+            break;
+        case 'rr':
+        case 'pp':
+        case 'ss':
+            console.log('Its a tie :P',userChoice, compChoice)
+            break;
+    }
 }
 
 function main() {
     rock_div.addEventListener('click', function() {
-        game('rock')
+        game('r')
     })
 
     paper_div.addEventListener('click', function() {
-        game('paper')
+        game('p')
     })
 
     scissors_div.addEventListener('click', function() {
-        game('scissors')
+        game('s')
     })
 }
 
