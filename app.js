068e2b18f6515d7327f1afad119d3f4c54421c67
Matt Wong <mattwong59@gmail.com>
2019-03-30 21:25:34 -0700

    Add border colour change to win/lost/tie functions

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const scissors_div = document.getElementById('scissors');
 
 
 function getCompChoice() {
-    const choices = ['Rock', 'Paper', 'Scissors'];
+    const choices = ['rock', 'paper', 'scissors'];
     const randomNum = Math.floor(Math.random() * 3);
     return choices[randomNum];
 }
@@ -21,38 +21,44 @@ function win(userChoice, compChoice) {
     userScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = compScore;    
-    result_div.innerHTML = `You win! ${userChoice} beats ${compChoice}.`;
+    result_div.innerHTML = `You win! Player: ${userChoice} beats Computer: ${compChoice}.`;
+    document.getElementById(userChoice).classList.add('green-glow');
+    setTimeout(function() {document.getElementById(userChoice).classList.remove('green-glow')}, 300) ;
 };
 
 function lost(userChoice, compChoice) {
     compScore++;
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = compScore;
-    result_div.innerHTML = `You lose! ${compChoice} beats ${userChoice}.`;
+    result_div.innerHTML = `You lose! Computer: ${compChoice} beats Player: ${userChoice}.`;
+    document.getElementById(userChoice).classList.add('red-glow');
+    setTimeout(function() {document.getElementById(userChoice).classList.remove('red-glow')}, 300) ;
 }
 
 function tie(userChoice, compChoice) {
     userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = compScore;
     result_div.innerHTML = `It's a tie.`;
+    document.getElementById(userChoice).classList.add('gray-glow');
+    setTimeout(function() {document.getElementById(userChoice).classList.remove('gray-glow')}, 300) ;
 }
 
 function game(userChoice) {
     let compChoice = getCompChoice();
     switch(userChoice + compChoice) {
-        case 'RockScissors':
-        case 'PaperRock':
-        case 'ScissorsPaper':
+        case 'rockscissors':
+        case 'paperrock':
+        case 'scissorspaper':
             win(userChoice, compChoice);
             break;
-        case 'PaperScissors':
-        case 'RockPaper':
-        case 'ScissorsRock':
+        case 'paperscissors':
+        case 'rockpaper':
+        case 'scissorsrock':
             lost(userChoice, compChoice);
             break;
-        case 'RockRock':
-        case 'PaperPaper':
-        case 'ScissorsScissors':
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorsscissors':
             tie(userChoice, compChoice);
             break;
     }
@@ -60,15 +66,15 @@ function game(userChoice) {
 
 function main() {
     rock_div.addEventListener('click', function() {
-        game('Rock')
+        game('rock')
     })
 
     paper_div.addEventListener('click', function() {
-        game('Paper')
+        game('paper')
     })
 
     scissors_div.addEventListener('click', function() {
-        game('Scissors')
+        game('scissors')
     })
 }
 
